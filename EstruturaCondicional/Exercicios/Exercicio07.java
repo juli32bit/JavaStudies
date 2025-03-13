a6ca8ca1e1ddc3ce74960a23cb64f2b4a4ae1fdc
Julia <juliamesqquita267@gmail.com>
2025-03-12 23:18:20 -0300

    Update Exercicio07.java

--- EstruturaCondicional/Exercicios/Exercicio07.java
+++ EstruturaCondicional/Exercicios/Exercicio07.java
@@ -1,7 +1,24 @@
 package EstruturaCondicional.Exercicios;
-
+import java.util.Scanner;
 public class Exercicio07 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+            
+        if (x == 0 && y == 0) {
+            System.out.println("origem");
+        }
+        else if (x > 0 && y > 0 ) {
+            System.out.println("Q1");
+        } else if (x < 0  && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0){
+            System.out.println("Q4");
+        }
     }
 }
